1c25b8a432b5ee6af9a8db520ce5c697ad2f0e87
Christian Decker <christian.decker@decker-software-solutions.de>
2017-06-10 15:40:59 +0200

    dynamic window size

--- gui/public/js/views/navbar.js
+++ gui/public/js/views/navbar.js
@@ -22,18 +22,37 @@ var NavBar = Backbone.View.extend({
     },
     toggleLogAreaView: function(event){
         event.preventDefault();
-        $('#log-area-panel').toggleClass('hidden');
+        $('#log-area').toggleClass('hidden');
         $('#log-area-view').find('.glyphicon-ok').toggleClass('hidden');
+        if($('#log-area').hasClass('hidden')){
+            $('.container-fluid').attr('style', 'height:calc(100% - 100px); margin-bottom:0px;');
+        }
+        else{
+            $('.container-fluid').attr('style', '');
+        }
     },
     toggleComponentAreaView: function(event){
         event.preventDefault();
-        $('#component-area-panel').toggleClass('hidden');
+        $('#component-area').toggleClass('hidden');
         $('#component-area-view').find('.glyphicon-ok').toggleClass('hidden');
+        this.setWorkAreaWidth();
     },
     toggleSettingsAreaView: function(event){
         event.preventDefault();
-        $('#settings-area-panel').toggleClass('hidden');
+        $('#settings-area').toggleClass('hidden');
         $('#settings-area-view').find('.glyphicon-ok').toggleClass('hidden');
+        this.setWorkAreaWidth();
+    },
+    setWorkAreaWidth: function(){
+        if($('#component-area').hasClass('hidden') && $('#settings-area').hasClass('hidden')){
+            $('#work-area').attr('class', 'col-lg-12 col-md-12 col-sm-12 col-xs-12');
+        }
+        else if($('#component-area').hasClass('hidden') || $('#settings-area').hasClass('hidden')){
+            $('#work-area').attr('class', 'col-lg-10 col-md-10 col-sm-10 col-xs-10');
+        }
+        else{
+            $('#work-area').attr('class', 'col-lg-8 col-md-8 col-sm-8 col-xs-8');
+        }
     },
     render: function() {
         this.$el.html(this.template);
