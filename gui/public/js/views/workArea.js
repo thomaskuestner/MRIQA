885bf7ed8cd01505016194b264169939b06f144c
Christian Decker <christian.decker@decker-software-solutions.de>
2017-06-12 11:01:41 +0200

    draw a little bit

--- gui/public/js/views/workArea.js
+++ gui/public/js/views/workArea.js
@@ -24,6 +24,12 @@ var WorkArea = Backbone.View.extend({
         var self = this;
         this.svg = d3.select('svg');
 
+        this.svg.call(d3.zoom()
+            .scaleExtent([0.2, 5])
+            .on('zoom', function(){
+                self.svg.attr('transform', d3.event.transform);
+            }));
+
         this.svg.attr('viewBox', `0 0 ${width} ${height}`)
             .attr('width', width)
             .attr('height', height)
@@ -34,7 +40,25 @@ var WorkArea = Backbone.View.extend({
 
         var pipeline = model.get('pipeline');
         if(pipeline.component){
-            model.get('pipeline').component.forEach(function(component) {
+            model.get('pipeline').component.forEach(function(component, index) {
+                if(typeof component.autoglue === 'undefined' || component.autoglue[0] === 'true'){
+                    var group = self.svg.append('g')
+                        .attr('transform', `translate(${index * 200},0)`);
+                    group.insert('rect','g')
+                        .attr('width', 150)
+                        .attr('height', 100)
+                        .attr('x', 0)
+                        .attr('y', 0)
+                        .attr('rx', 2)
+                        .attr('ry', 2)
+                        .style('fill-opacity', 0)
+                        .style('stroke','black')
+                        .style('stroke-width', 1);
+                    group.insert('text','g')
+                        .attr('x', 10)
+                        .attr('y', 50)
+                        .text(component.name[0]);
+                }
             }, this);
         }
     }
